95667bef5b59d347057ff82b7cbe5274c35d292e
Siddhant Priyadarshi <siddhantp4ever@gmail.com>
2022-07-08 00:08:39 +0530

    Leetcode

--- LC_ReverseaNumber.py
+++ LC_ReverseaNumber.py
@@ -1,8 +1,27 @@
+#Problem: https://leetcode.com/problems/reverse-integer/
+
+reversed_num = 0
+        while x != 0:
+            reversed_num = (reversed_num * 10) + (x % 10)
+            x //= 10
+        return reversed_num
+    
+    
+    
+$--------------------------------------------------------------------------------#
+
+
+#Optimised
 class Solution:
     def reverse(self, x: int) -> int:
-        rev=''
-        while(x):
-            y=x%10
-            rev=rev+str(y)
-            x=x//10
-        return rev
+        isNegative = False
+        if x < 0:
+            isNegative = True
+            x = -x
+        reversedNumber = 0
+        while x:
+            reversedNumber = reversedNumber * 10 + x % 10
+            x //= 10
+        if reversedNumber >= 2 ** 31 - 1 or reversedNumber <= -2 ** 31:
+            return 0
+        return -reversedNumber if isNegative else reversedNumber
