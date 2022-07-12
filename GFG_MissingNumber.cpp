cb34eaaffb54e985a66058bd60991346e8b9be2e
Siddhant Priyadarshi <siddhantp4ever@gmail.com>
2022-07-12 22:29:29 +0530

    Update GFG_MissingNumber.cpp

--- GFG_MissingNumber.cpp
+++ GFG_MissingNumber.cpp
@@ -9,3 +9,16 @@ class Solution{
         }
     }
 };
+
+
+
+//Another Method
+int getMissingNo(int a[], int n)
+{
+    int n_elements_sum = n * (n + 1) / 2;
+    int sum = 0;
+
+    for (int i = 0; i < n - 1; i++)
+        sum += a[i];
+    return n_elements_sum - sum;
+}
