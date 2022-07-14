4a4550a13480893f0175e2668ca12501d9de516c
Siddhant Priyadarshi <siddhantp4ever@gmail.com>
2022-07-14 19:12:02 +0530

    GeeksForGeeks

--- GFG_SortArrayOf-0-1-2.cpp
+++ GFG_SortArrayOf-0-1-2.cpp
@@ -31,3 +31,29 @@ class Solution
     }
     
 };
+
+
+/* -----------------------------------------------------------------------*/
+
+//Array sorting Method
+
+void sort012(int a[], int n)
+   {
+     int mid=0;
+     int low=0;
+     int high=n-1;
+     while(mid<=high){
+         if(a[mid]==0){
+          swap(a[mid],a[low]);
+           low++;
+           mid++;
+         }
+          else if (a[mid]==2){
+            swap(a[mid],a[high]);
+            high--;
+         }
+         else{
+             mid++;
+         }
+     }
+   }
