96efa02e459737751fa4ace413cab06957a298b0
Siddhant Priyadarshi <siddhantp4ever@gmail.com>
2022-07-14 18:03:04 +0530

    GeeksForGeeks

--- GFG_SubarrayWithGivenSum.cpp
+++ GFG_SubarrayWithGivenSum.cpp
@@ -3,27 +3,29 @@
 class Solution
 {
     public:
-    //Function to find a continuous sub-array which adds up to a given number.
     vector<int> subarraySum(int arr[], int n, long long s)
     {
         int sum=0, k=0;
         vector<int> sub;
+        
         for(int i=0;i<n;i++){
             
-            if(sum<s)
-                sum+=arr[i];
+            sum+=arr[i];
             
-            if(sum>s){
-                sum-=arr[k];
-                k++;
+            if(sum>=s){
+                while(sum>s){
+                    sum-=arr[k];
+                    k++;
+                }
+                if(sum==s){
+                    sub.push_back(k+1);
+                    sub.push_back(i+1);
+                    return sub;
+                } 
             }
-            
-            if(sum==s){
-                sub.push_back(k+1);
-                sub.push_back(i+1);
-                return sub;
-            } 
         }
+        if(sub.empty())
+            sub.push_back(-1);
         return sub;
     }
 };
