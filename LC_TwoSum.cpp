8162dc386b8345bb62019001414567de8027eb0b
Siddhant Priyadarshi <siddhantp4ever@gmail.com>
2022-07-15 17:28:24 +0530

    LeetCode

--- LC_TwoSum.cpp
+++ LC_TwoSum.cpp
@@ -3,18 +3,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> lookUp;
-        vector<int> ans;
-        
+        vector<int> v;
+        unordered_map<int, int> m;
         for(int i=0;i<nums.size();i++){
-            int complement=target-nums[i];
-            if(lookUp.find(complement)!=lookUp.end()){
-                ans.push_back(lookUp[complement]);
-                ans.push_back(i);
+            int b = target-nums[i];
+            if(m.find(b)!=m.end()){
+                v.push_back(m[b]);
+                v.push_back(i);
                 break;
-            }else{
-                lookUp.insert(make_pair(nums[i], i));
             }
-        }return ans;
+            m.insert(make_pair(nums[i], i));
+        }
+        return v;
     }
 };
