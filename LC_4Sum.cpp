9ae6329d3681e7381353f72d35f3f3403abfe3b4
Siddhant Priyadarshi <siddhantp4ever@gmail.com>
2022-07-19 12:58:44 +0530

    LeetCode

--- LC_4Sum.cpp
+++ LC_4Sum.cpp
@@ -11,27 +11,20 @@ public:
         vector<vector<int>> v;
         
         for(int i=0; i<n-3; i++){
-            /*if(nums[i]<0 || nums[i]>=n)
-                continue;*/
             long long int target1=target-nums[i];
             
             for(int j=i+1;j<n-2;j++){
-                /*if(nums[j]<0 || nums[j]>=n)
-                    continue;*/
                 long long int target2=target1-nums[j];
                 int low=j+1;
                 int high=n-1;
 
                 while(low<high){
-                    /*if((nums[low]<0 || nums[low]>=n) && (nums[high]<0 || nums[high]>=n))
-                        continue;*/
-
                     sum=nums[low]+nums[high];
 
                     if(sum<target2)
                         low++;
                     else if(sum>target2)
-                        high++;
+                        high--;
                     else {
                         v.push_back({nums[i], nums[j], nums[low], nums[high]});
                         int current_low=nums[low];
@@ -44,11 +37,11 @@ public:
                             high--;
                     }
                 }
-                /*while(j+1<n && nums[j+1] == nums[j]) 
-                    j++;*/
+                while(j+1<n && nums[j+1] == nums[j]) 
+                    j++;
             }
-            /*while(i+1<n && nums[i+1] == nums[i]) 
-                i++;*/
+            while(i+1<n && nums[i+1] == nums[i]) 
+                i++;
         }
         return v;
     }
