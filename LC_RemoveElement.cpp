2ce057313646a91d8790763438d9bfa53f7a15e4
Siddhant Priyadarshi <siddhantp4ever@gmail.com>
2022-07-19 22:42:41 +0530

    LeetCode

--- LC_RemoveElement.cpp
+++ LC_RemoveElement.cpp
@@ -1,5 +1,19 @@
 //Problem: https://leetcode.com/problems/remove-element/
 
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0, j = nums.size() - 1;
+        while (i <= j) {
+            if(nums[i] == val) {
+                if (nums[j] != val) nums[i++] = nums[j--];
+                else j--;
+            }
+            else i++;
+        }
+        return i;
+    }
+};
 
 /*-------------------------------------------------------------*/
 //Python solution
