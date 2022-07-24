679566cd467a336ef40eb5880c56fd806f2bdb6e
Siddhant Priyadarshi <siddhantp4ever@gmail.com>
2022-07-24 11:43:01 +0530

    GeeksForGeeks - Matrix

---
+++ GFG_BoundaryTraversalofMatrix.cpp
@@ -0,0 +1,37 @@
+//Problem: 
+
+class Solution
+{   
+    public:
+    //Function to return list of integers that form the boundary 
+    //traversal of the matrix in a clockwise manner.
+    vector<int> boundaryTraversal(vector<vector<int> > matrix, int n, int m) 
+    {
+        vector<int> output;
+        
+        if(n == 1){
+            int i = 0;
+            while(i < m)
+                output.push_back(matrix[0][i++]);
+        }
+        else if(m == 1){
+            int i = 0;
+            while(i < n)
+                output.push_back(matrix[i++][0]);
+        }
+        else{
+            for(int j=0; j<m; j++)
+                output.push_back(matrix[0][j]);
+            
+            for(int j=1; j<n; j++)
+                output.push_back(matrix[j][m-1]);
+            
+            for(int j=m-2; j>=0; j--)
+                output.push_back(matrix[n-1][j]);
+            
+            for(int j=n-2; j>=1; j--)
+                output.push_back(matrix[j][0]);
+        }
+        return output;
+    }
+};
